744a46eb87074d1cc5bd5de6458d0c4d90f9bf4e
Mr. Danger <johnnydollard@gmail.com>
2018-05-02 15:54:48 -0500

    Update processInput.py

--- processInput.py
+++ processInput.py
@@ -8,14 +8,21 @@ import Adafruit_PCA9685#Servos and Linear Actuators
 adc = Adafruit_ADS1x15.ADS1115()
 pwm = Adafruit_PCA9685.PCA9685()
 
-GAIN = 1 #Set the gain. This needs to be experimented with to find the most reliable results.
+GAIN = 1 #Set the gain. 
 channel = 0 #set the channel for analog input
 
 pwm.set_pwm_freq(60)#frequency good for servos
 inputValues = [5000,5000,5000,5000,5000]#initialize the list at of the last 5 inputs at low values
 
-#note: Apart from the equations used to set their positions, servos and linear actuators are interchangeable.
-
+with open('equationValues.txt', 'r') as f:
+	lines = f.readlines()
+	scaleFactor = float(lines[0])
+	offsetAmount = float(lines[1])
+	outputMin = int(lines[2])
+	outputMax = int(lines[3])
+	print(scaleFactor, offsetAmount, outputMin, outputMax)
+	
+	
 print('Reading ADS1x15 values, press Ctrl-C to quit...')
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 
@@ -24,26 +31,16 @@ while True:#execution loop
     inputValues.insert(0, adc.read_adc(channel, gain=GAIN))#add the most recent input to the front of the list
     inputValues.pop()#remove the least recent input from the end of the list
     
-    #equations calculated at https://www.desmos.com/calculator/6ftevgrchz
-    
-    #servos
-    #outputValue = int(sorted(inputValues)[2]/47+114) #convert ADC input to servo output
-    '''if outputValue<200:
-	       outputValue = 200
-	   if outputValue>665:
-	       outputValue = 665'''
-	       
-	#linear actuators
-	#outputValue = int(sorted(inputValues)[2]/76+208) #same as above, but for linear actuator
-    '''if outputValue<260:
-	       outputValue = 260
-	   if outputValue>550:
-	       outputValue = 550'''
+    outputValue = int(sorted(inputValues)[2]/scaleFactor+offsetAmount) #convert ADC input to servo output
+    if outputValue<outputMin:
+	    outputValue = outputMin
+    if outputValue>outputMax:
+        outputValue = outputMax
 	       
 	#manual input: 
     #outputValue = int(input("Output Value: "))
 	
-    print (outputValue)#display value servo has been set to
+    #print (outputValue)#display value servo has been set to
     print (inputValues[2])#display median of 5 most recent input values (used to calculate where the servo is set)
     
     pwm.set_pwm(0, 0, int(outputValue))#thumb
